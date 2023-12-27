b93da3088d0e0cd64a6ba853996f834f48c65c04
Antoine <antoine@2ohm.fr>
2023-12-27 21:50:55 +0100

    Fine tune rythm

--- build_midi.sh
+++ build_midi.sh
@@ -10,13 +10,13 @@ psalm=$2
 
 # BUILD
 # Add the midi section
-sed -e 's/\\score {/\\score { \\midi { \\tempo 1 = 60 }/g' "psaumes/${office}/${psalm}.ly" | \
+sed -e 's/\\score {/\\score { \\midi { \\tempo 1 = 90 }/g' "psaumes/${office}/${psalm}.ly" | \
 # Remove flex \
 sed '/\+/d' | \
 # Add rest before bars \
 sed 's/\\bar "|"/r1 \\bar "|"/g' | \
 # Longer \
 sed 's/\\breve/1/g' | \
-sed 's/4$$/1/g' | \
+sed 's/4$/2/g' | \
 #sed 's/4/2/g' | \
 lilypond $options_midi -o "build/psaumes/${office}/midi/${psalm}_psalmodie" -;
